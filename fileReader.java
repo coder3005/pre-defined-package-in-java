40c88c5546533edfeb7b8bba0c9f3ebb3e32e49e
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2023-07-11 11:42:44 +0530

    Create fileReader.java

---
+++ fileReader.java
@@ -0,0 +1,30 @@
+/* Read Data from a File */
+import java.Lang.System;
+import java.Lang.String;
+
+import java.io.File;
+import java.io.IOException;
+
+import java.util.Scanner;
+
+class fileReader
+{
+    public static void main(String[] args) {
+        
+        try
+        {
+            File r=new File("C:\\Users\\Abhi Gupta\\downloads\\UP.txt");
+            Scanner sc=new Scanner(r);
+            while(sc.hasNextLine())
+            {
+                System.out.println(sc.hasNextLine());
+                System.out.println(sc.nextLine());
+                System.out.println(sc.hasNextLine());
+            }
+        }
+        catch(IOException e)
+        {
+            System.out.println("Exception Handled...!");
+        }
+    }
+}
